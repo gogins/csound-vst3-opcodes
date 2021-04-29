1231136971a4c4e8047ba9483296a694e79f23ac
Michael Gogins <michael.gogins@gmail.com>
2021-04-29 10:32:52 -0400

    Missing nmake for Windows (#5).

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -4,5 +4,5 @@ rm -rf ./build-windows
 mkdir -p build-windows
 cd build-windows
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
-make clean
-make VERBOSE=1 j=4
+nmake clean
+nmake VERBOSE=1 j=4
