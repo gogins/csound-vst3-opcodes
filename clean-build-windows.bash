be17fa3cc83ebca330a26ed11e1b3bed5637ff40
Michael Gogins <michael.gogins@gmail.com>
2021-05-06 12:53:21 -0400

    Windows...

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -3,5 +3,7 @@ echo "Making a clean build of csound-vst3 for debugging with optimization..."
 rm -rf ./build-windows
 mkdir -p build-windows
 cd build-windows
-cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
-nmake
+cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vs
+cd build-windows
+dir
+MSBuild.exe
