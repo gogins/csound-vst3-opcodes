afd09ccf4596bb1e277c9e3393dcca04c7843325
Michael Gogins <michael.gogins@gmail.com>
2021-05-06 13:27:38 -0400

    Update clean-build-windows.bash

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -4,5 +4,6 @@ rm -rf ./build-windows
 mkdir -p build-windows
 cd build-windows
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
+cmake --build .
 dir
 MSBuild.exe
