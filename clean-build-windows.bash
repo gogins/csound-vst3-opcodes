0d3bf4e779468c7da6974f70dd77fb34e64fce0c
Michael Gogins <michael.gogins@gmail.com>
2021-05-06 12:16:13 -0400

    Changed to nmake.

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -4,4 +4,4 @@ rm -rf ./build-windows
 mkdir -p build-windows
 cd build-windows
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
-cmake --build
+nmake
