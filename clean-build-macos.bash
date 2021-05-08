8728d5f46a579ac55ea7142a5e3f5add3e9d3232
Michael Gogins <michael.gogins@gmail.com>
2021-05-08 09:35:13 -0400

    Update clean-build-macos.bash
    Changing filename extension for Mac OS.

--- clean-build-macos.bash
+++ clean-build-macos.bash
@@ -6,14 +6,10 @@ cd build-macos
 # cmake ../vst3sdk  -DCMAKE_CXX_FLAGS=-I/usr/local/include -DCMAKE_EXE_LINKER_FLAGS=/Library/Frameworks/CsoundLib64.framework/CsoundLib64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -GXcode
 cmake ../vst3sdk  -DCMAKE_CXX_FLAGS=-I/usr/local/include -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -GXcode
 xcodebuild
+mv csound-vst3-opcodes/build/lib/Debug/libvst3_plugins.so csound-vst3-opcodes/build/lib/Debug/libvst3_plugins.dylib
 echo "Finding vst3_plugins:"
 find . -name "*vst3_plugins*" -ls
 echo "Completed a clean build of csound-vst3."
-
-# The last stage is to copy the plugin to Csound's plugin directory, and
-# correct its name. We used
-#
-# cp csound-vst3-opcodes/build/lib/Debug/libvst3_plugins.so Frameworks/CsoundLib64.framework/Versions/6.0/Resources/Opcodes64/libvst3_plugins.dylib
-#
-#This location may differ on your system.
+# Install to: Frameworks/CsoundLib64.framework/Versions/6.0/Resources/Opcodes64/libvst3_plugins.dylib
+# This location may differ on your system.
 
