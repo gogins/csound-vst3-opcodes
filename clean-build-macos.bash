158837b974c5000e1c02014d4fbac8d570cf54b7
Michael Gogins <michael.gogins@gmail.com>
2021-05-08 09:50:50 -0400

    Update clean-build-macos.bash

--- clean-build-macos.bash
+++ clean-build-macos.bash
@@ -6,7 +6,7 @@ cd build-macos
 # cmake ../vst3sdk  -DCMAKE_CXX_FLAGS=-I/usr/local/include -DCMAKE_EXE_LINKER_FLAGS=/Library/Frameworks/CsoundLib64.framework/CsoundLib64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -GXcode
 cmake ../vst3sdk  -DCMAKE_CXX_FLAGS=-I/usr/local/include -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -GXcode
 xcodebuild
-mv csound-vst3-opcodes/build/lib/Debug/libvst3_plugins.so csound-vst3-opcodes/build/lib/Debug/libvst3_plugins.dylib
+mv lib/Debug/libvst3_plugins.so lib/Debug/libvst3_plugins.dylib
 echo "Finding vst3_plugins:"
 find . -name "*vst3_plugins*" -ls
 echo "Completed a clean build of csound-vst3."
