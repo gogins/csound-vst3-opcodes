2842d0ddd8f41d64d17fcc5a01927e22e491646a
Michael Gogins <michael.gogins@gmail.com>
2021-05-10 15:09:30 -0400

    Continuous build and release (#5).

--- clean-build-linux.bash
+++ clean-build-linux.bash
@@ -6,7 +6,7 @@ cd build-linux
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
 make clean
 make VERBOSE=1 j=4
-find . -type f -name "*.so" -exec cp {} . \;
+find .. -type f -name "*.so" -exec cp {} . \;
 echo "Built plugins:"
 pwd
 ls -ll
