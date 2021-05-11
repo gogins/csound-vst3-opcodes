a84beef585b8623f06316d9da43533c1d7adfa6b
Michael Gogins <michael.gogins@gmail.com>
2021-05-10 20:29:39 -0400

    Bring in .vst3 directories (#5).

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -6,9 +6,13 @@ cd build-windows
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=RelWithDebug -SMTG_CREATE_PLUGIN_LINK=0 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
 dir
 cmake --build .
-find .. -type f -name "*.DLL" -exec cp {} . \;
-find .. -type f -name "*.dll" -exec cp {} . \;
-find .. -type f -name "*.pdb" -exec cp {} . \;
+find . -type f -name "*.DLL" -exec cp {} . \;
+find . -type f -name "*.dll" -exec cp {} . \;
+find . -type f -name "*.pdb" -exec cp {} . \;
+find . -name "*.vst3" -exec cp -rf {} . \;
+echo "Built plugins:"
+pwd
+ls -ll
 echo "Built plugins:"
 pwd
 ls -ll 
