a49510b1d48b72886c9b75dac9f66c90fba6bece
Michael Gogins <michael.gogins@gmail.com>
2021-05-10 21:08:27 -0400

    Update clean-build-windows.bash

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -3,7 +3,7 @@ echo "Making a clean build of csound-vst3..."
 rm -rf ./build-windows
 mkdir -p build-windows
 cd build-windows
-cmake ../vst3sdk -DCMAKE_BUILD_TYPE=RelWithDebug -SMTG_CREATE_PLUGIN_LINK=0 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
+cmake ../vst3sdk -DCMAKE_BUILD_TYPE=RelWithDebug -DSMTG_CREATE_PLUGIN_LINK=0 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
 dir
 cmake --build .
 find . -type f -name "*.DLL" -exec cp {} . \;
