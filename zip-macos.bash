b83260f01938f7082ddc0144a92213d3882e5893
Michael Gogins <michael.gogins@gmail.com>
2021-05-11 09:13:20 -0400

    Archiving (#5).

--- zip-macos.bash
+++ zip-macos.bash
@@ -3,13 +3,13 @@ echo "Making a release archive for Mac OS..."
 export ARCHIVE=csound-vst3-macos.zip
 export TARGETS=build-macos
 rm -f $ARCHIVE
-zip $ARCHIVE README.md
-zip $ARCHIVE LICENSE
-zip $ARCHIVE *.png
-zip $ARCHIVE csound-vst3/vst3-opcodes/README.md
-zip $ARCHIVE csound-vst3/vst3-opcodes/*.png
-zip $ARCHIVE csound-vst3/vst3-opcodes/*.csd
-zip $ARCHIVE $TARGETS/*.so
-zip $ARCHIVE $TARGETS/*.dylib
-zip $ARCHIVE -r $TARGETS/*.vst3
+7za a $ARCHIVE README.md
+7za a $ARCHIVE LICENSE
+7za a $ARCHIVE *.png
+7za a $ARCHIVE csound-vst3/vst3-opcodes/README.md
+7za a $ARCHIVE csound-vst3/vst3-opcodes/*.png
+7za a $ARCHIVE csound-vst3/vst3-opcodes/*.csd
+7za a $ARCHIVE $TARGETS/*.so
+7za a $ARCHIVE $TARGETS/*.dylib
+7za a $ARCHIVE -r $TARGETS/*.vst3
 echo "Completed the release archive for Linux."
--- zip-windows.bash
+++ zip-windows.bash
@@ -3,13 +3,13 @@ echo "Making a release archive for Windows..."
 export ARCHIVE=csound-vst3-windows.zip
 export TARGETS=build-windows
 rm -f $ARCHIVE
-zip $ARCHIVE README.md
-zip $ARCHIVE LICENSE
-zip $ARCHIVE *.png
-zip $ARCHIVE csound-vst3/vst3-opcodes/README.md
-zip $ARCHIVE csound-vst3/vst3-opcodes/*.png
-zip $ARCHIVE csound-vst3/vst3-opcodes/*.csd
-zip $ARCHIVE $TARGETS/*.dll
-zip $ARCHIVE $TARGETS/*.pdb
-zip $ARCHIVE -r $TARGETS/*.vst3
+7za a $ARCHIVE README.md
+7za a $ARCHIVE LICENSE
+7za a $ARCHIVE *.png
+7za a $ARCHIVE csound-vst3/vst3-opcodes/README.md
+7za a $ARCHIVE csound-vst3/vst3-opcodes/*.png
+7za a $ARCHIVE csound-vst3/vst3-opcodes/*.csd
+7za a $ARCHIVE $TARGETS/*.dll
+7za a $ARCHIVE $TARGETS/*.pdb
+7za a $ARCHIVE -r $TARGETS/*.vst3
 echo "Completed the release archive for Windows."
