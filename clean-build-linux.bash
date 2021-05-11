f903ea8cc2ab904dcf2d098ef02020848b855f12
Michael Gogins <michael.gogins@gmail.com>
2021-05-11 14:27:30 -0400

    List archive contents, better glob to catch plugin target.

--- clean-build-linux.bash
+++ clean-build-linux.bash
@@ -6,7 +6,5 @@ cd build-linux
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
 make clean
 make VERBOSE=1 j=4
-echo "Built:"
-find . -name "*vst3_plugins*" -ls
 cd ..
 echo "Completed a clean build of csound-vst3."
--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -6,8 +6,6 @@ cd build-windows
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=RelWithDebug -DSMTG_CREATE_PLUGIN_LINK=0 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
 dir
 cmake --build .
-echo "Built:"
-find . -name "*vst3_plugins*" -ls
 cd ..
 echo "Completed a clean build of csound-vst3."
 
