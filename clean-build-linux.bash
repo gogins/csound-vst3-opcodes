0bb422cc31902f94a8a5c866442a768add84db1d
Michael Gogins <michael.gogins@gmail.com>
2021-05-11 20:26:20 -0400

    Yikes...

--- clean-build-linux.bash
+++ clean-build-linux.bash
@@ -9,6 +9,8 @@ mkdir -p build-linux
 export MYPLUGINS_PATH=$(pwd)/csound-vst3
 echo "MYPLUGINS_PATH: $MYPLUGINS_PATH"
 cd build-linux
+pwd
+ls ../csound-vst3
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
 make clean
 make VERBOSE=1 j=4
