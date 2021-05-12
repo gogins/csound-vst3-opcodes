1d7b1b68c8a0c29c3dd3b9c05fb8089785547a8f
Michael Gogins <michael.gogins@gmail.com>
2021-05-12 15:17:18 -0400

    Building for release not debug (#5).

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -5,7 +5,7 @@ mkdir -p build-windows
 export MYPLUGINS_PATH=$(pwd)/csound-vst3
 echo "MYPLUGINS_PATH: ${MYPLUGINS_PATH}"
 cd build-windows
-cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH:PATH=${MYPLUGINS_PATH}
+cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH:PATH=${MYPLUGINS_PATH}
 cmake --build .
 cd ..
 echo "Completed a clean build of csound-vst3."
