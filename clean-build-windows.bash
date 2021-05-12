a48ea80e361c47b058bda1bf48b4460c71990a98
Michael Gogins <michael.gogins@gmail.com>
2021-05-12 15:43:09 -0400

    Update clean-build-windows.bash

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -2,10 +2,8 @@
 echo "Making a clean build of csound-vst3..."
 rm -rf build-windows
 mkdir -p build-windows
-export MYPLUGINS_PATH=$(pwd)/csound-vst3
-echo "MYPLUGINS_PATH: ${MYPLUGINS_PATH}"
 cd build-windows
-cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH:PATH=${MYPLUGINS_PATH}
+cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
 cmake --build .
 cd ..
 echo "Completed a clean build of csound-vst3."
