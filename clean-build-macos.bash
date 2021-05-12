7fb9dd7b841c231ac45d455d4f8796ffed70b6dc
Michael Gogins <michael.gogins@gmail.com>
2021-05-12 18:52:26 -0400

    Building for release not debug (#5).

--- clean-build-macos.bash
+++ clean-build-macos.bash
@@ -6,7 +6,7 @@ export MYPLUGINS_PATH=$(pwd)/csound-vst3
 echo "MYPLUGINS_PATH: ${MYPLUGINS_PATH}"
 cd build-macos
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH:PATH=${MYPLUGINS_PATH} -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -GXcode
-xcodebuild
+xcodebuild 
 mv lib/Debug/libvst3_plugins.so lib/Debug/libvst3_plugins.dylib
 cd ..
 echo "Completed a clean build of csound-vst3."
