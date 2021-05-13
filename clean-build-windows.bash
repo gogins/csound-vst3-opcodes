1526831545aaa26a9f8b2d1312ee77023a4e9580
Michael Gogins <michael.gogins@gmail.com>
2021-05-12 20:27:06 -0400

    --build --config Release may be preferred (#5).

--- clean-build-windows.bash
+++ clean-build-windows.bash
@@ -4,7 +4,7 @@ rm -rf build-windows
 mkdir -p build-windows
 cd build-windows
 cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAGS=-O2 -DSMTG_MYPLUGINS_SRC_PATH=../csound-vst3
-cmake --build . --config Release -verbose --parallel 4
+cmake --build . --config Release --verbose --parallel 4
 cd ..
 echo "Completed a clean build of csound-vst3."
 
