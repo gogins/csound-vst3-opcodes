2313fa3a36a43dfc82f7b6264170f4982156d526
Michael Gogins <michael.gogins@gmail.com>
2021-11-06 14:12:12 -0400

    #2.

--- clean-build-macos.bash
+++ clean-build-macos.bash
@@ -9,7 +9,7 @@ cmake ../vst3sdk -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-O2 -DCMAKE_CXX_FLAG
 
 xcodebuild
 
-mv lib/Release/libvst3_plugins.so lib/Release/libvst3_plugins.dylib
+mv lib/Debug/libvst3_plugins.so lib/Debug/libvst3_plugins.dylib
 cd ..
 echo "Completed a clean build of csound-vst3."
 echo "Install to: Frameworks/CsoundLib64.framework/Versions/6.0/Resources/Opcodes64/libvst3_plugins.dylib."
