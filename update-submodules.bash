8d73e46d78cc7b55f14da45f98419e10a76f77dc
Michael Gogins <michael.gogins@gmail.com>
2021-11-06 12:56:35 -0400

    Update update-submodules.bash

--- update-submodules.bash
+++ update-submodules.bash
@@ -1,6 +1,6 @@
 #!/bin/bash 
 echo "Updating all submodules for csound-vst3-opcodes..."
-git submodule update --init --recursive --remote
+git submodule --init --recursive --remote
 git submodule status --recursive
 sudo -k
 echo "Finished updating all submodules for csound-vst3-opcodes."
