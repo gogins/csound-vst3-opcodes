8189a0fdc02eeb024a380b125d97d11eab7ce5ef
gogins <michael.gogins@gmail.com>
2022-03-01 11:10:45 +0100

    Trying again to remove submodule for Csound (#9).

--- zip-macos.bash
+++ zip-macos.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "Making a release archive for Mac OS..."
+echo "Making a release archive for macOS..."
 export ARCHIVE=csound-vst3-macos.zip
 export TARGETS=build-macos
 rm -f $ARCHIVE
-7za a $ARCHIVE README.md
-7za a $ARCHIVE LICENSE
-7za a $ARCHIVE *.png
-7za a $ARCHIVE csound-vst3/vst3-opcodes/README.md
-7za a $ARCHIVE csound-vst3/vst3-opcodes/*.png
-7za a $ARCHIVE csound-vst3/vst3-opcodes/*.csd
-7za a $ARCHIVE -r $TARGETS/*vst3_plugins*
-7za a $ARCHIVE -r $TARGETS/*.vst3
+7zz a $ARCHIVE README.md
+7zz a $ARCHIVE LICENSE
+7zz a $ARCHIVE *.png
+7zz a $ARCHIVE csound-vst3/vst3-opcodes/README.md
+7zz a $ARCHIVE csound-vst3/vst3-opcodes/*.png
+7zz a $ARCHIVE csound-vst3/vst3-opcodes/*.csd
+7zz a $ARCHIVE -r $TARGETS/*vst3_plugins*
+7zz a $ARCHIVE -r $TARGETS/*.vst3
 echo "Archived:"
-7za l $ARCHIVE
-echo "Completed the release archive for Linux."
+7zz l $ARCHIVE
+echo "Completed the release archive for macOS."
