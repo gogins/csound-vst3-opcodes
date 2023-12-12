945a756722135ed0f014273ca0253c93d0584efc
gogins <michael.gogins@gmail.com>
2023-12-12 13:51:56 -0500

    Ensure csound6 branch (#9).

--- update-submodules.bash
+++ update-submodules.bash
@@ -3,4 +3,9 @@ echo "Updating all submodules for csound-vst3-opcodes..."
 git submodule update --init --recursive
 git submodule status --recursive
 sudo -k
+cd csound
+git pull
+git checkout csound6
+git branch
+cd ..
 echo "Finished updating all submodules for csound-vst3-opcodes."
