8f5214345d82537760ab3c1df849b4292878e878
gogins <michael.gogins@gmail.com>
2023-12-12 14:14:05 -0500

    Consistent release archives (#9).

--- zip-linux.bash
+++ zip-linux.bash
@@ -3,14 +3,17 @@ echo "Making a release archive for Linux..."
 export ARCHIVE=csound-vst3-linux.zip
 export TARGETS=build-linux
 rm -f $ARCHIVE
-7za a $ARCHIVE README.md
-7za a $ARCHIVE LICENSE
-7za a $ARCHIVE *.png
-7za a $ARCHIVE csound-vst3/vst3-opcodes/README.md
-7za a $ARCHIVE csound-vst3/vst3-opcodes/*.png
-7za a $ARCHIVE csound-vst3/vst3-opcodes/*.csd
-7za a $ARCHIVE -r $TARGETS/*vst3_plugins*
-7za a $ARCHIVE -r $TARGETS/*.vst3
+7z a $ARCHIVE README.md
+7z a $ARCHIVE LICENSE
+7z a $ARCHIVE *.png
+7z a $ARCHIVE vst3-opcodes/README.md
+7z a $ARCHIVE vst3-opcodes/*.png
+7z a $ARCHIVE vst3-opcodes/*.csd
+7z a $ARCHIVE vst3-opcodes/README.md
+7z a $ARCHIVE examples/*.csd
+7z a $ARCHIVE examples/*.*preset
+7z a $ARCHIVE -r $TARGETS/*vst3_plugins*
+7z a $ARCHIVE -r $TARGETS/*.vst3
 echo "Archived:"
-7za l $ARCHIVE
+7z l $ARCHIVE
 echo "Completed the release archive for Linux."
