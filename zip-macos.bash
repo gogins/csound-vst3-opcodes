308893715a32bcd385dbd4c4742989e3f8696326
gogins <michael.gogins@gmail.com>
2023-12-14 08:33:22 -0500

    Tweaking GitHub builds (#14).

--- zip-macos.bash
+++ zip-macos.bash
@@ -12,8 +12,8 @@ rm -f $ARCHIVE
 7z a $ARCHIVE vst3-opcodes/README.md
 7z a $ARCHIVE examples/*.csd
 7z a $ARCHIVE examples/*.*preset
-7z a $ARCHIVE -r $TARGETS/*vst3_plugins*
-7z a $ARCHIVE -r $TARGETS/*.vst3
+7z a $ARCHIVE -r libvst3_plugins*
+7z a $ARCHIVE -r $TARGETS/VST3/*
 echo "Archived:"
 7z l $ARCHIVE
 echo "Completed the release archive for macOS."
--- zip-windows.bash
+++ zip-windows.bash
@@ -2,7 +2,6 @@
 echo "Making a release archive for Windows..."
 export ARCHIVE=csound-vst3-windows.zip
 export TARGETS=build-windows
-export RUNNER_TARGETS=D:/a/csound-vst3-opcodes/csound-vst3-opcodes/build-windows
 rm -f $ARCHIVE
 7z a $ARCHIVE README.md
 7z a $ARCHIVE LICENSE
@@ -13,8 +12,8 @@ rm -f $ARCHIVE
 7z a $ARCHIVE vst3-opcodes/README.md
 7z a $ARCHIVE examples/*.csd
 7z a $ARCHIVE examples/*.*preset
-7z a $ARCHIVE -r $TARGETS/*.dll
-7z a $ARCHIVE -r $RUNNER_TARGETS/*.dll
-7z o "Archived:"
+7z a $ARCHIVE -r libvst3_plugins*
+7z a $ARCHIVE -r $TARGETS/VST3/*
+echo "Archived:"
 7z l $ARCHIVE
 echo "Completed the release archive for Windows."
