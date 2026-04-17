e586286987daf11965e1debf6834eb89d4566468
gogins <michael.gogins@gmail.com>
2026-04-17 06:48:23 -0400

    Porting to Csound 7.

--- clean-build-macos.bash
+++ clean-build-macos.bash
@@ -1,25 +1,37 @@
 #!/bin/bash
 clear
 echo "Starting a clean build of csound-vst3-opcodes for macOS..."
+
 find . -name CMakeCache.txt -delete
 rm -rf build-macos
 mkdir -p build-macos
 cd build-macos
-# A lot of patching is needed here, I wonder why.
-# A lot of patching is needed here, I wonder why.
+
+CSOUND_FRAMEWORK_DIR="$HOME/Library/Frameworks"
+CSOUND_FRAMEWORK="$CSOUND_FRAMEWORK_DIR/CsoundLib64.framework"
+CSOUND_INCLUDE_DIR="$CSOUND_FRAMEWORK/Headers"
+CSOUND_LIBRARY="$CSOUND_FRAMEWORK/CsoundLib64"
+
 cmake ../vst3-opcodes \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DSMTG_BUILD_UNIVERSAL_BINARY=1 \
     -DCMAKE_C_FLAGS="-O2" \
     -DCMAKE_CXX_FLAGS="-Wno-error=unused-but-set-variable -Wno-error=deprecated-declarations" \
-    -DCMAKE_SHARED_LINKER_FLAGS="-framework Foundation" \
-    -DCMAKE_EXE_LINKER_FLAGS="-framework Foundation" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-framework Foundation -F$CSOUND_FRAMEWORK_DIR -Wl,-rpath,$CSOUND_FRAMEWORK_DIR" \
+    -DCMAKE_EXE_LINKER_FLAGS="-framework Foundation -F$CSOUND_FRAMEWORK_DIR -Wl,-rpath,$CSOUND_FRAMEWORK_DIR" \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+    -DCMAKE_PREFIX_PATH="$HOME/Library;$HOME/Library/Frameworks" \
+    -DCMAKE_FRAMEWORK_PATH="$CSOUND_FRAMEWORK_DIR" \
+    -DCSOUND_INCLUDE_DIR="$CSOUND_INCLUDE_DIR" \
+    -DCSOUND_INCLUDE_DIRS="$CSOUND_INCLUDE_DIR" \
+    -DCSOUND_LIBRARY="$CSOUND_LIBRARY" \
+    -DCSOUND_LIBRARIES="$CSOUND_LIBRARY" \
     -G "Unix Makefiles"
-    
+
 cmake --build . --verbose --parallel 4
+
 mv lib/RelWithDebInfo/libvst3_plugins.so lib/RelWithDebInfo/libvst3_plugins.dylib
+
 cd ..
 find . -name "libvst3_plugins.*" -ls 2>/dev/null
 echo "Completed a clean build of csound-vst3-opcodes for macOS."
-
